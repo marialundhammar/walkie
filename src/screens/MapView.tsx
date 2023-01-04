ce9227c7b9b555377a026c3facfdbb4ad0f3e064
Maria Lundhammar <marialundhammar@Marias-MacBook-Pro.local>
2023-01-04 12:56:57 +0100

    audio not working, checking if it's something with version of expo

--- src/screens/MapView.tsx
+++ src/screens/MapView.tsx
@@ -1,20 +1,19 @@
 import { StyleSheet, SafeAreaView, Text, Button } from 'react-native';
 import Map from '../components/Map';
 import { Audio } from 'expo-av';
+import React, { Component } from 'react';
 import { useEffect, useState } from 'react';
 
 const HomeView = () => {
+  let sound = new Audio.Sound();
+
+  sound.loadAsync(require('../assets/sample-3s.mp3'));
+  console.log('loaded');
+
   const playSound = async () => {
-    console.log('clicked');
-
-    const { sound: playbackObject } = await Audio.Sound.createAsync(
-      require('../assets/sample-3s.mp3'),
-      { shouldPlay: true }
-    );
-
-    console.log(playbackObject);
-    await playbackObject.playAsync;
     console.log('playing');
+    await sound.playAsync();
+    await sound.unloadAsync();
   };
 
   return (
